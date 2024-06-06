ab3c3e61131596ac9ee5f0a6720730a7dc3a5a8c
rianwpzx <rianwp2015@gmail.com>
2024-06-06 15:40:44 +0700

    add manage patient page and detail patient page

--- Capstone-React-6/src/App.jsx
+++ Capstone-React-6/src/App.jsx
@@ -1,31 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { isAuthenticated } from './utils/auth';
-import Login from './pages/Login/Login';
-import Homepage from './pages/Home/Homepage';
-import ManageContent from './pages/ManageContent/ManageContent';
-import ManagePatient from './pages/ManagePatient/ManagePatient';
-import Transaction from './pages/Transaction/Transaction';
+import React, { useState, useEffect } from "react"
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from "react-router-dom"
+// import { isAuthenticated } from "./utils/auth"
+import Login from "./pages/Login/Login"
+import Homepage from "./pages/Home/Homepage"
+import ManageContent from "./pages/ManageContent/ManageContent"
+import ManagePatient from "./pages/ManagePatient/ManagePatient"
+import Transaction from "./pages/Transaction/Transaction"
+import Layout from "./components/Layout"
+import Detail from "./pages/ManagePatient/Detail"
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(true)
 
-  useEffect(() => {
-    // Check if the user is logged in by checking the token
-    setIsLoggedIn(isAuthenticated());
-  }, []);
+	useEffect(() => {
+		// Check if the user is logged in by checking the token
+		// setIsLoggedIn(isAuthenticated())
+	}, [])
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={isLoggedIn ? <Navigate to="/homepage" /> : <Login />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/managecontent" element={<ManageContent />} />
-        <Route path="/managepatient" element={<ManagePatient />} />
-        <Route path="/transaction" element={<Transaction />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route
+					path="/"
+					element={isLoggedIn ? <Layout /> : <Navigate to="/login" />}
+				>
+					<Route path="homepage" element={<Homepage />} />
+					<Route path="managecontent" element={<ManageContent />} />
+					<Route path="managepatient" element={<ManagePatient />} />
+					<Route path="managepatient/detail" element={<Detail />} />
+					<Route path="transaction" element={<Transaction />} />
+				</Route>
+			</Routes>
+		</Router>
+	)
 }
 
-export default App;
+export default App
--- Capstone-React-6/src/pages/Home/Homepage.jsx
+++ Capstone-React-6/src/pages/Home/Homepage.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { logout } from '../../utils/auth';
-import { Navigate } from 'react-router-dom';
+import { logout } from "../../utils/auth"
+import { Navigate } from "react-router-dom"
 
 export default function Homepage() {
-  const handleLogout = () => {
-    logout();
-    // Redirect to login page after logout
-    <Navigate to="/Login" />;
-  };
+	const handleLogout = () => {
+		logout()
+		// Redirect to login page after logout
+		;<Navigate to="/Login" />
+	}
 
-  return (
-    <div>
-      <h1>Welcome to Homepage</h1>
-      <button className='btn' onClick={handleLogout}>Logout</button>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Welcome to Homepage</h1>
+			<button className="btn" onClick={handleLogout}>
+				Logout
+			</button>
+		</div>
+	)
 }
